712687d9ada6d916011f87b89f8a7018cae08bb5
Shaun Collins <shaunc311@gmail.com>
2015-01-21 19:30:20 -0500

    added links map

---
+++ lib/elixir_adn/links.ex
@@ -0,0 +1,12 @@
+defmodule ElixirADN.Links do
+	@doc ~S"""
+	An elixir representation of the ADN links map found in the User map
+	{
+    "len": 7,
+    "pos": 31,
+    "text": "App.net",
+    "url": "http://App.net"
+  }
+	"""
+	defstruct len: -1, pos: -1, text: "", url: ""
+end

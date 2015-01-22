b55e3da4b939ca94c7dac97dda9d850c5a0e3bf9
Shaun Collins <shaunc311@gmail.com>
2015-01-21 19:08:44 -0500

    added the user image map

---
+++ lib/elixir_adn/image.ex
@@ -0,0 +1,12 @@
+defmodule ElixirADN.Image do
+	@doc ~S"""
+	An elixir representation of the ADN image map found in the User map
+	{
+    "height": 200,
+    "is_default": false,
+    "url": "https://d2rfichhc2fb9n.cloudfront.net/image/5/aoveeP73f33UcFhyhqzn7VhwgS17InMiOiJzMyIsImIiOiJhZG4tdXNlci1hc3NldHMiLCJrIjoiYXNzZXRzL3VzZXIvOTkvYTYvNDAvOTlhNjQwMDAwMDAwMDAwMC5wbmciLCJvIjoiIn0",
+    "width": 200
+	}
+	"""
+	defstruct height: -1, is_default: false, url: "", width: -1
+end

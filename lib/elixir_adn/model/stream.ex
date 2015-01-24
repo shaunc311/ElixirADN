48e02d86b2b34ee99e569c442e16865a9f485515
Shaun Collins <shaunc311@gmail.com>
2015-01-23 23:06:06 -0500

    added stream map

---
+++ lib/elixir_adn/model/stream.ex
@@ -0,0 +1,16 @@
+defmodule ElixirADN.Model.Stream do
+	@doc ~S"""
+	An elixir representation of the ADN stream map
+	{
+	  "endpoint": "https://stream-channel.app.net...",
+	  "filter": "...filter object...",
+	  "id": "1",
+	  "object_types": [
+	    "post"
+	  ],
+	  "type": "long_poll",
+	  "key": "rollout_stream"
+	}
+	"""
+	defstruct endpoint: "", filter: nil, id: "", object_types: [], type: "", key: ""
+end

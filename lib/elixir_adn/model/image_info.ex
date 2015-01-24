e15974aa2f5dccd431f7ea614c4bf8632c8c9cc7
Shaun Collins <shaunc311@gmail.com>
2015-01-23 22:58:20 -0500

    added image info

---
+++ lib/elixir_adn/model/image_info.ex
@@ -0,0 +1,10 @@
+defmodule ElixirADN.Model.ImageInfo do
+	@doc ~S"""
+	An elixir representation of the ADN image info map
+	{
+    "width": 200,
+    "height": 200
+  }
+	"""
+	defstruct width: -1, height: -1
+end

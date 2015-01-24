1625a211b1eee334cad00fadc923d16dc7607dca
Shaun Collins <shaunc311@gmail.com>
2015-01-23 22:38:54 -0500

    added the channel permissions map

---
+++ lib/elixir_adn/model/channel_permissions.ex
@@ -0,0 +1,15 @@
+defmodule ElixirADN.Model.ChannelPermissions do
+	@doc ~S"""
+	An elixir representation of the ADN channel reader/writer/editor permissions
+	{
+    "any_user": false,
+    "immutable": false,
+    "public": true,
+    "user_ids": [
+
+    ],
+    "you": true
+  }
+	"""
+	defstruct any_user: false, immutable: false, public: false, user_ids: [], you: false 
+end

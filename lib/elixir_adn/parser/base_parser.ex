a2b139ec81aeb144f7f6db008e0ce476ec4117f9
Shaun Collins <shaunc311@gmail.com>
2015-01-30 19:26:09 -0500

    added model aliases to base_parser

--- lib/elixir_adn/parser/base_parser.ex
+++ lib/elixir_adn/parser/base_parser.ex
@@ -1,4 +1,24 @@
 defmodule ElixirADN.Parser.BaseParser do
+	alias ElixirADN.Model.Annotation
+	alias ElixirADN.Model.Channel
+	alias ElixirADN.Model.ChannelCounts
+	alias ElixirADN.Model.ChannelPermissions
+	alias ElixirADN.Model.DerivedFiles
+	alias ElixirADN.Model.Description
+	alias ElixirADN.Model.Entities
+	alias ElixirADN.Model.File
+	alias ElixirADN.Model.Hashtag
+	alias ElixirADN.Model.Image
+	alias ElixirADN.Model.ImageInfo
+	alias ElixirADN.Model.ImageThumb
+	alias ElixirADN.Model.Link
+	alias ElixirADN.Model.Mention
+	alias ElixirADN.Model.Message
+	alias ElixirADN.Model.Post
+	alias ElixirADN.Model.Source
+	alias ElixirADN.Model.User
+	alias ElixirADN.Model.UserCounts
+
 
 	@doc ~S"""
 	Parse post data coming back from ADN.  It's mapped data => values
@@ -49,79 +69,79 @@ defmodule ElixirADN.Parser.BaseParser do
 	end
 
 	#Decode all the children properties from the post object
-	defp decode_children(%ElixirADN.Model.Post{} = post) do
+	defp decode_children(%Post{} = post) do
 		post
-			|> Map.put( :entities, decode(:entities, post.entities, ElixirADN.Model.Entities))
-			|> Map.put( :source, decode(:source, post.source, ElixirADN.Model.Source))
-			|> Map.put( :user, decode(:user, post.user, ElixirADN.Model.User))
-			|> Map.put( :annotations, decode(:annotation, post.annotations, ElixirADN.Model.Annotation))
-			|> Map.put( :reposters, decode(:users, post.reposters, ElixirADN.Model.User))
-			|> Map.put( :starred_by, decode(:users, post.starred_by, ElixirADN.Model.User))
+			|> Map.put( :entities, decode(:entities, post.entities, Entities))
+			|> Map.put( :source, decode(:source, post.source, Source))
+			|> Map.put( :user, decode(:user, post.user, User))
+			|> Map.put( :annotations, decode(:annotation, post.annotations, Annotation))
+			|> Map.put( :reposters, decode(:users, post.reposters, User))
+			|> Map.put( :starred_by, decode(:users, post.starred_by, User))
 	end
 
 	#Decode all the children properties from the user object
-	defp decode_children(%ElixirADN.Model.User{} = user) do
+	defp decode_children(%User{} = user) do
 		user
-			|> Map.put( :avatar_image, decode(:image, user.avatar_image, ElixirADN.Model.Image))
-			|> Map.put( :counts,  decode(:user_counts,user.counts, ElixirADN.Model.UserCounts))
-			|> Map.put( :cover_image, decode(:image, user.cover_image, ElixirADN.Model.Image))
-			|> Map.put( :description, decode(:description, user.description, ElixirADN.Model.Description))
-			|> Map.put( :annotations, decode(:annotation, user.annotations, ElixirADN.Model.Annotation))
+			|> Map.put( :avatar_image, decode(:image, user.avatar_image, Image))
+			|> Map.put( :counts,  decode(:user_counts,user.counts, UserCounts))
+			|> Map.put( :cover_image, decode(:image, user.cover_image, Image))
+			|> Map.put( :description, decode(:description, user.description, Description))
+			|> Map.put( :annotations, decode(:annotation, user.annotations, Annotation))
 	end
 
 	#Decode all the children properties from the entities object
-	defp decode_children(%ElixirADN.Model.Entities{} = entities) do
+	defp decode_children(%Entities{} = entities) do
 		entities
-			|> Map.put( :hashtags, decode(:hashtags, entities.hashtags, ElixirADN.Model.Hashtag))
-			|> Map.put( :links,  decode(:links,entities.links, ElixirADN.Model.Link))
-			|> Map.put( :mentions, decode(:mentions, entities.mentions, ElixirADN.Model.Mention))
+			|> Map.put( :hashtags, decode(:hashtags, entities.hashtags, Hashtag))
+			|> Map.put( :links,  decode(:links,entities.links, Link))
+			|> Map.put( :mentions, decode(:mentions, entities.mentions, Mention))
 	end
 
 	#Decode all the children properties from the description object
-	defp decode_children(%ElixirADN.Model.Description{} = description) do
+	defp decode_children(%Description{} = description) do
 		description
-			|> Map.put( :entities, decode(:entities, description.entities, ElixirADN.Model.Entities))
+			|> Map.put( :entities, decode(:entities, description.entities, Entities))
 	end
 
 	#Decode all the children properties from the channel object
-	defp decode_children(%ElixirADN.Model.Channel{} = channel) do
+	defp decode_children(%Channel{} = channel) do
 		channel
-			|> Map.put( :counts, decode(:counts, channel.counts, ElixirADN.Model.ChannelCounts))
-			|> Map.put( :readers, decode(:readers, channel.readers, ElixirADN.Model.ChannelPermissions))
-			|> Map.put( :editors, decode(:editors, channel.editors, ElixirADN.Model.ChannelPermissions))
-			|> Map.put( :writers, decode(:writers, channel.writers, ElixirADN.Model.ChannelPermissions))
-			|> Map.put( :recent_message, decode(:recent_message, channel.recent_message, ElixirADN.Model.Message))
-			|> Map.put( :annotations, decode(:annotation, channel.annotations, ElixirADN.Model.Annotation))
+			|> Map.put( :counts, decode(:counts, channel.counts, ChannelCounts))
+			|> Map.put( :readers, decode(:readers, channel.readers, ChannelPermissions))
+			|> Map.put( :editors, decode(:editors, channel.editors, ChannelPermissions))
+			|> Map.put( :writers, decode(:writers, channel.writers, ChannelPermissions))
+			|> Map.put( :recent_message, decode(:recent_message, channel.recent_message, Message))
+			|> Map.put( :annotations, decode(:annotation, channel.annotations, Annotation))
 	end
 
 	#Decode all the children properties from the message object
-	defp decode_children(%ElixirADN.Model.Message{} = message) do
+	defp decode_children(%Message{} = message) do
 		message
-			|> Map.put( :entities, decode(:entities, message.entities, ElixirADN.Model.Entities))
-			|> Map.put( :source, decode(:source, message.source, ElixirADN.Model.Source))
-			|> Map.put( :user, decode(:user, message.user, ElixirADN.Model.User))
-			|> Map.put( :annotations, decode(:annotation, message.annotations, ElixirADN.Model.Annotation))
+			|> Map.put( :entities, decode(:entities, message.entities, Entities))
+			|> Map.put( :source, decode(:source, message.source, Source))
+			|> Map.put( :user, decode(:user, message.user, User))
+			|> Map.put( :annotations, decode(:annotation, message.annotations, Annotation))
 	end
 
 	#Decode all the children properties from the file object
-	defp decode_children(%ElixirADN.Model.File{} = file) do
+	defp decode_children(%File{} = file) do
 		file
-			|> Map.put( :derived_files, decode(:derived_files, file.derived_files, ElixirADN.Model.DerivedFiles))
-			|> Map.put( :image_info, decode(:image_info, file.image_info, ElixirADN.Model.ImageInfo))
-			|> Map.put( :source, decode(:source, file.source, ElixirADN.Model.Source))
-			|> Map.put( :user, decode(:user, file.user, ElixirADN.Model.User))
+			|> Map.put( :derived_files, decode(:derived_files, file.derived_files, DerivedFiles))
+			|> Map.put( :image_info, decode(:image_info, file.image_info, ImageInfo))
+			|> Map.put( :source, decode(:source, file.source, Source))
+			|> Map.put( :user, decode(:user, file.user, User))
 	end
 
 	#Decode all the children properties from the derived file object
-	defp decode_children(%ElixirADN.Model.DerivedFiles{} = files) do
+	defp decode_children(%DerivedFiles{} = files) do
 		files
-			|> Map.put( :image_thumb_200s, decode(:image_thumb, files.image_thumb_200s, ElixirADN.Model.ImageThumb))
-			|> Map.put( :image_thumb_960r, decode(:image_thumb, files.image_thumb_960r, ElixirADN.Model.ImageThumb))
+			|> Map.put( :image_thumb_200s, decode(:image_thumb, files.image_thumb_200s, ImageThumb))
+			|> Map.put( :image_thumb_960r, decode(:image_thumb, files.image_thumb_960r, ImageThumb))
 	end
 
-	defp decode_children(%ElixirADN.Model.ImageThumb{} = thumb) do
+	defp decode_children(%ImageThumb{} = thumb) do
 		thumb
-			|> Map.put( :image_info, decode(:image_info, thumb.image_info, ElixirADN.Model.ImageInfo))
+			|> Map.put( :image_info, decode(:image_info, thumb.image_info, ImageInfo))
 	end
 
 	#Decode all the children properties from the description object

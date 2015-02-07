cfdc2fc86b3b23e62498d1978945bf69a3c88d7e
Shaun Collins <shaunc311@gmail.com>
2015-02-07 08:36:32 -0500

    updated docs for http

--- lib/elixir_adn/endpoints/http.ex
+++ lib/elixir_adn/endpoints/http.ex
@@ -1,20 +1,26 @@
 defmodule ElixirADN.Endpoints.Http do
 	alias ElixirADN.Parser.StatusParser
-	
-	#A general function to call an http method.  This should be in it's own
-	#module eventually
+	@moduledoc ~S"""
+	A helper utility to send a request to a url and process it
+	"""
+
+	@doc ~S"""
+	A general function to get an http request and process the result.
+	"""
 	def call({:get, url}, headers) when is_list(headers) do
 		HTTPotion.get(url, headers)
 			|> read_response
 	end
 
-	#A general function to call an http method.  This should be in it's own
-	#module eventually
+	@doc ~S"""
+	A general function to post to an http endpoint and process the result.
+	"""
 	def call({:post, url}, headers) when is_list(headers) do
 		HTTPotion.post(url, headers)
 			|> read_response
 	end
 
+	#Parse the status code that comes back
 	defp read_response(%HTTPotion.Response{ status_code: code } = success_message) do
 		success = StatusParser.parse_status(code)
 		case success do

1a5e5511e14347828217e12169b6984f67429392
Shaun Collins <shaunc311@gmail.com>
2015-03-01 11:19:16 -0500

    added reading posts from global

--- lib/elixir_adn/endpoints/post.ex
+++ lib/elixir_adn/endpoints/post.ex
@@ -1,7 +1,10 @@
 defmodule ElixirADN.Endpoints.Post do
 	alias ElixirADN.Endpoints.Http
 	alias ElixirADN.Endpoints.Parameters.Encoder
+	alias ElixirADN.Endpoints.Parameters.Pagination
+	alias ElixirADN.Endpoints.Parameters.PostParameters
 	alias ElixirADN.Model.Post
+	alias ElixirADN.Parser.ResultParser
 
 	@moduledoc ~S"""
 	An interface to the posts endpoints in ADN.  They are urls begining with 
@@ -18,6 +21,36 @@ defmodule ElixirADN.Endpoints.Post do
 
 	def create_post(_,_) do
 		{:error, :invalid_object_to_post}
+	end
+
+	@doc ~S"""
+	Get posts from the global stream
+	"""
+	def get_posts(%PostParameters{} = post_parameters, %Pagination{} = pagination) do
+		#Make sure the string starts with @
+		results = process_get_posts(post_parameters, pagination)
+			|> parse_to_posts
+		
+		{:ok, results}
+	end
+
+	#Parse the response body into a map and then into objects
+	defp parse_to_posts(%HTTPoison.Response{body: body}) do
+		{result, value} = ResultParser.parse(:posts, body)
+		case result do
+			:ok -> ResultParser.decode(:posts, value, Post)
+			:error -> {:error, value}
+		end
+	end
+
+	#Turn the parameters into a query string and then make the get call
+	defp process_get_posts(%PostParameters{} = post_parameters, %Pagination{} = pagination ) do
+		query_string_result = Encoder.generate_query_string([post_parameters, pagination])
+
+		case query_string_result do
+			{:ok, query_string} -> Http.call({:get, "https://api.app.net/posts/stream/global#{query_string}"}, [])
+			error -> error
+		end	
 	end
 
 	#Call the post endpoint with the json post
